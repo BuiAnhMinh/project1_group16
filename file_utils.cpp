014e5a08f39211a4ed22edcfcaa38332b1e7325f
Cooper Agustin <cooper.agustin@protonmail.com>
2025-09-01 21:23:05 +1000

    feat: removed error messages, optimised

--- file_utils.cpp
+++ file_utils.cpp
@@ -4,7 +4,6 @@ bool directory_exists(const std::string& dir){
     if (std::filesystem::exists(dir) && std::filesystem::is_directory(dir)){
         return true;
     } else {
-        std::cerr << "Directory does not exist: " << dir << std::endl;
         return false;
     }
 }
@@ -13,20 +12,18 @@ bool file_exists(const std::string& file_path){
     if (std::filesystem::exists(file_path) && std::filesystem::is_regular_file(file_path)){
         return true;
     } else {
-        std::cerr << "File does not exist: " << file_path << std::endl;
         return false;
     }
 }
 
 bool check_directory_or_create(const std::string& dir){
-    if (!std::filesystem::exists(dir) || !std::filesystem::is_directory(dir)) {
-        try {
-            std::filesystem::create_directories(dir);
-            return directory_exists(dir);
-        } catch (const std::exception& e){
-            std::cerr << "Error creating directory: " << e.what() << std::endl;
-            return false;
-        }
+    if (std::filesystem::exists(dir) && std::filesystem::is_directory(dir)) {
+        return true;
     }
-    return true;
+    try {
+        std::filesystem::create_directories(dir);
+        return true;
+    } catch (const std::exception& e){
+        return false;
+    }
 }
